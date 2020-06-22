05e245003832b55e0df3b8e5442cb6050629186f
Yogesh <myogeshchavan97@gmail.com>
2020-06-22 07:51:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.get('/', async (req, res) => {
+  const username = req.query.username || 'myogeshchavan97';
+  try {
+    const result = await axios.get(
+      `https://api.github.com/users/${username}/repos`
+    );
+    const repos = result.data.map((repo) => ({
+      name: repo.name,
+      url: repo.html_url,
+      description: repo.description
+    }));
+
+    res.send(repos);
+  } catch (error) {
+    res.status(400).send('Error while getting list of repositories');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
